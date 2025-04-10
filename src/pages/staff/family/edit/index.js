700053a6a37d954f48d7d82ac2dd3857b523c8a6
Harish Devarayal <harish.devarayal@nichi.com>
2025-04-10 13:03:56 +0530

    driving lic step change

--- src/pages/staff/family/edit/index.js
+++ src/pages/staff/family/edit/index.js
@@ -768,7 +768,7 @@ const handleScan = async () => {
   }
 
    const ivuResult = async () => {
-     if(window.location.origin === "https://rakuraku.nichi.in"){
+     //if(window.location.origin === "https://rakuraku.nichi.in"){
        try{
          setLoader(true);
        const res = await fetchIvuResponse();
@@ -817,54 +817,54 @@ const handleScan = async () => {
          console.log(err)
  
        }
-     }
-     else {
-     setLoader(true);
-     let payload = {
-       client_url:"http://10.8.0.6:50080"
-     }
-     ivuToolRegistration(payload, async (res) => {
-       if (res) {
-         const evacueeArray = res.data.data;
-         let newEvacuee = createEvacuee(evacueeArray);
-         newEvacuee = {
-           ...newEvacuee,
-           isFromFormReader: true
-         };
-         if (!newEvacuee.postalCode || !evacueeArray.prefecture_id) {
-           const address = evacueeArray.fullAddress || evacueeArray.address;
-           try {
-             const { prefecture, postalCode, prefecture_id } = await geocodeAddressAndExtractData(address, localeJson, locale, setLoader);
+  //    }
+  //    else {
+  //    setLoader(true);
+  //    let payload = {
+  //      client_url:"http://10.8.0.6:50080"
+  //    }
+  //    ivuToolRegistration(payload, async (res) => {
+  //      if (res) {
+  //        const evacueeArray = res.data.data;
+  //        let newEvacuee = createEvacuee(evacueeArray);
+  //        newEvacuee = {
+  //          ...newEvacuee,
+  //          isFromFormReader: true
+  //        };
+  //        if (!newEvacuee.postalCode || !evacueeArray.prefecture_id) {
+  //          const address = evacueeArray.fullAddress || evacueeArray.address;
+  //          try {
+  //            const { prefecture, postalCode, prefecture_id } = await geocodeAddressAndExtractData(address, localeJson, locale, setLoader);
 
-             // Update newEvacuee with geocoding data
-             newEvacuee = {
-               ...newEvacuee,
-               postalCode: postalCode,
-               prefecture_id: prefecture_id
-             };
-           } catch (error) {
-             console.error("Error fetching geolocation data:", error);
-           }
-         }
-         setEditObj(newEvacuee)
-         setRegisterModalAction("edit");
-         setSpecialCareEditOpen(true);
-         setEvacuee((prev) => {
-           return [
-             ...prev, // Use spread operator to include previous items in the array
-             newEvacuee, // Add the newEvacuee to the array
-           ];
-         });
-         formikRef.current.setFieldValue("evacuee", [
-           ...formikRef.current.values.evacuee,
-           newEvacuee,
-         ]);
-         setLoader(false);
-       } else {
-         setLoader(false);
-       }
-     });
-   }
+  //            // Update newEvacuee with geocoding data
+  //            newEvacuee = {
+  //              ...newEvacuee,
+  //              postalCode: postalCode,
+  //              prefecture_id: prefecture_id
+  //            };
+  //          } catch (error) {
+  //            console.error("Error fetching geolocation data:", error);
+  //          }
+  //        }
+  //        setEditObj(newEvacuee)
+  //        setRegisterModalAction("edit");
+  //        setSpecialCareEditOpen(true);
+  //        setEvacuee((prev) => {
+  //          return [
+  //            ...prev, // Use spread operator to include previous items in the array
+  //            newEvacuee, // Add the newEvacuee to the array
+  //          ];
+  //        });
+  //        formikRef.current.setFieldValue("evacuee", [
+  //          ...formikRef.current.values.evacuee,
+  //          newEvacuee,
+  //        ]);
+  //        setLoader(false);
+  //      } else {
+  //        setLoader(false);
+  //      }
+  //    });
+  //  }
    };
 
   const handleRecordingStateChange = (isRecord) => {
