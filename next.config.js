161a46882106caab5a5be9cabe9f0d6373567cc4
Harish Devarayal <harish.devarayal@nichi.com>
2025-05-12 10:34:18 +0530

    remove CSP Headers

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const cspHeader = `
   default-src 'self' https://rakuraku.nichi.in/ https://hinanjo.nichi.in/ https://demo.hinan.telenet.co.jp/;
+  font-src 'self' https://fonts.googleapis.com/ https://fonts.gstatic.com/; 
   script-src 'self' 'unsafe-inline' 'unsafe-eval' https://docs.opencv.org/ https://maps.googleapis.com/ https://static.line-scdn.net/liff/edge/2/non-ios-extensions_2_22_0.js;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/;
-  font-src 'self' https://fonts.googleapis.com/ https://fonts.gstatic.com/;
   connect-src 'self' data: https://fonts.gstatic.com/ http://127.0.0.1:50080 ws://127.0.0.1:17778 https://rakuraku.nichi.in/ https://hinanjo.nichi.in/ https://rakurakuapi.nichi.in/api/ https://api.hinanjo.nichi.in/api/ https://demo.hinan.telenet.co.jp/ https://api.demo.hinan.telenet.co.jp/api/ https://maps.googleapis.com/  https://api.line.me/ https://liffsdk.line-scdn.net/;
   img-src 'self' data: https://maps.googleapis.com/ https://maps.gstatic.com/ https://rakurakuapi.nichi.in/ https://hinanjo.nichi.in/ https://demo.hinan.telenet.co.jp/ https://dummyimage.com/;
   frame-ancestors 'self';
   form-action 'self';
   worker-src 'self' blob:;
 `;
+
 
 const nextConfig = {
   poweredByHeader: false,
@@ -30,10 +31,10 @@ const nextConfig = {
 
   async headers() {
     const securityHeaders = [
-      {
-        key: 'Content-Security-Policy',
-        value: cspHeader.replace(/\s{2,}/g, ' ').trim(),
-      },
+      // {
+      //   key: 'Content-Security-Policy',
+      //   value: cspHeader.replace(/\s{2,}/g, ' ').trim(),
+      // },
       {
         key: 'X-Frame-Options',
         value: 'SAMEORIGIN',
